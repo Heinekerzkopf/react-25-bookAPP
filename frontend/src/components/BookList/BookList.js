469d14a2ee166312bd8b06c25bf6f1f4af089593
Ervin Koroman <ervin.koroman12@gmail.com>
2024-04-10 12:32:25 +0200

    initialize delete book functionality

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,15 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteBook } from '../../redux/books/actionCreators';
 import './BookList.css'
 
 const BookList = () => {
     const books = useSelector((state) => state.books)
+    const dispatch = useDispatch();
+
+    const deleteBookHandler = (id) => {
+        dispatch(deleteBook(id))
+    }
+
     return (
         <div className="app-block book-list">
             <h2>Book List</h2>
@@ -13,6 +20,7 @@ const BookList = () => {
                     {books.map((book, i) => (
                         <li key={book.id}>
                             <div className='book-info'>{++i}. {book.title} by <strong>{book.author}</strong></div>
+                            <div className='book-actions'><button onClick={() => deleteBookHandler(book.id)}>DELETE</button></div>
                         </li>
                     ))}
                 </ul>
